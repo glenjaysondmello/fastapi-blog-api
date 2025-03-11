f941e6fddb2a302b82d3bef2d3faa7cd4fb96d04
Glen Jayson Dmello <glendmello04@gmail.com>
2025-03-11 23:57:53 +0530

    applied exception and status code

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,7 +15,7 @@ def get_db():
 
 models.Base.metadata.create_all(engine)
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def blogPost(request: schemas.Blog, db : Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -28,9 +28,13 @@ def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get("/blog/{id}")
-def show(id, db : Session = Depends(get_db)):
+@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
+def show(id, response: Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"Details from ID: {id} is not Found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"Details from ID: {id} is not Found"}
     return blog
 
 
