2f1b51d87d0952a1e60a06c1e114c8897bb2c7d6
Glen Jayson Dmello <glendmello04@gmail.com>
2025-03-13 20:14:52 +0530

    applied delete and update blog

--- blog/main.py
+++ blog/main.py
@@ -37,4 +37,22 @@ def show(id, response: Response, db : Session = Depends(get_db)):
         # return {"message": f"Details from ID: {id} is not Found"}
     return blog
 
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"Details from ID: {id} is not Found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {"Done"}
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"Details from ID: {id} is not Found")
+    blog.update(request.model_dump(), synchronize_session=False)
+    db.commit()
+    return "updated"
+
 
