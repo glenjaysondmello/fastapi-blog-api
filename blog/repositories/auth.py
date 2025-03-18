69e5565d73dd86901f9dd56c9021a3929b60b7ca
Glen Jayson Dmello <glendmello04@gmail.com>
2025-03-18 20:02:42 +0530

    applied jwt access token

--- blog/repositories/auth.py
+++ blog/repositories/auth.py
@@ -1,5 +1,5 @@
 from fastapi import status, Response, HTTPException
-from .. import schemas, models
+from .. import schemas, models, token
 from sqlalchemy.orm import Session
 from ..hashing import Hash
 
@@ -9,4 +9,6 @@ def login(request: schemas.Login, db: Session):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
     if not Hash.verify(request.password, user.password):
         raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail=f"Incorrect Password")
-    return user
+    access_token = token.create_access_token(data={"sub": user.email})
+    return schemas.Token(access_token=access_token, token_type="bearer")
+
--- blog/schemas.py
+++ blog/schemas.py
@@ -45,6 +45,14 @@ class Login(BaseModel):
     username: str
     password: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
 
 
     
