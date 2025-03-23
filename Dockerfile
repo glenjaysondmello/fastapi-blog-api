1565a2cc7c94f6c2fc02dee4f108abd8b7fc89d4
Glen Jayson Dmello <glendmello04@gmail.com>
2025-03-23 23:55:41 +0530

    dockerized the application

---
+++ Dockerfile
@@ -0,0 +1,58 @@
+# FROM python:3.11-slim
+
+# WORKDIR /appfastapi
+
+# COPY requirements.txt .
+
+# RUN pip install --upgrade pip && \
+#     pip install -r requirements.txt
+
+# COPY . .
+
+# EXPOSE 8000
+
+# ENV PYTHONUNBUFFERED=1 \
+#     PYTHONDONTWRITEBYTECODE=1
+
+# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+
+# FROM python:3.11-slim
+
+# ENV PYTHONUNBUFFERED=1 \
+#     PYTHONDONTWRITEBYTECODE=1
+
+# RUN mkdir -p /home/appfastapi
+
+# WORKDIR /home/appfastapi
+
+# COPY ./blog/requirements.txt /home/appfastapi/requirements.txt
+
+# RUN pip install --upgrade pip && \
+#     pip install -r requirements.txt
+
+# COPY ./blog /home/appfastapi
+
+# EXPOSE 8000
+
+# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+
+
+FROM python:3.9
+
+WORKDIR /code
+
+COPY ./blog/requirements.txt /code/requirements.txt
+
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+COPY ./blog /code/app
+
+EXPOSE 8000
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-from fastapi import FastAPI
-from typing import Optional
-from pydantic import BaseModel
-import uvicorn
+# from fastapi import FastAPI
+# from typing import Optional
+# from pydantic import BaseModel
+# import uvicorn
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get('/blog')
-def blog(limit = 10, published: bool = True, sort: Optional[str] = None):
-    if published:
-        return {"data": f"{limit} published blogs"}
-    else:
-        return {"data": f"{limit} blogs"}
+# @app.get('/blog')
+# def blog(limit = 10, published: bool = True, sort: Optional[str] = None):
+#     if published:
+#         return {"data": f"{limit} published blogs"}
+#     else:
+#         return {"data": f"{limit} blogs"}
 
-@app.get('/blog/unpublished')
-def unpublished():
-    return {"data": "All unpublished blogs"}
+# @app.get('/blog/unpublished')
+# def unpublished():
+#     return {"data": "All unpublished blogs"}
 
-@app.get('/blog/{id}')
-def blogId(id: int):
-    return {"data": id}
+# @app.get('/blog/{id}')
+# def blogId(id: int):
+#     return {"data": id}
 
-class Blog(BaseModel):
-    title: str
-    body: str
-    published: Optional[bool] = True
+# class Blog(BaseModel):
+#     title: str
+#     body: str
+#     published: Optional[bool] = True
 
-@app.post('/blog')
-def create_blog(blog: Blog):
-    return {"data": f"Blog is created with title as {blog.title}"}
+# @app.post('/blog')
+# def create_blog(blog: Blog):
+#     return {"data": f"Blog is created with title as {blog.title}"}
 
-@app.get("/blog/{id}/comments")
-def comments(limit = 10):
-    return limit
-    # return {"data": {"1", "2"}}
+# @app.get("/blog/{id}/comments")
+# def comments(limit = 10):
+#     return limit
+#     # return {"data": {"1", "2"}}
 
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="127.0.0.1", port=9000)
+# # if __name__ == "__main__":
+# #     uvicorn.run(app, host="127.0.0.1", port=9000)
